db72d7c8437571c03addf4bd1023fe3887c69b57
Saidur289 <saidur.riaz1@gmailcomm>
2025-01-09 20:25:10 +0600

    create route for payment

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 require('dotenv').config()
+var morgan = require('morgan')
+const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.PAYMENT_KEY);
+const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
-app.use(cors())
 app.use(express.json())
-
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}))
+app.use(cookieParser())
+app.use(morgan('dev'))
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -27,11 +36,84 @@ async function run() {
     const menuCollection = client.db('bossDB').collection('menu')
     const reviewCollection = client.db('bossDB').collection('reviews')
     const cartsCollection = client.db('bossDB').collection('carts')
-  
+    const usersCollection = client.db('bossDB').collection('users')
+    const paymentsCollection = client.db('bossDB').collection('payments')
+    app.post('/jwt', async(req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '365d'})
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      }).send({success: true})
+    })
+    // logout 
+    app.get('/logout', async(req, res) => {
+      try{
+        res.clearCookie('token', {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        }).send({success: true})
+      }
+      catch (err) {
+        res.status(500).send(err)
+      }
+    })
+    const verifyToken = async (req, res, next) => {
+      const token = req.cookies?.token 
+      if(!token) return res.status(401).send({ message: 'unauthorized access' })
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if(err){
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      req.user = decoded
+      next()
+      })
+    }
+    // verify admin verify
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.user?.email 
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      const isAdmin = user?.role === 'admin'
+      if(!isAdmin){
+        return res.status(403).send({message: 'Forbidden Access'})
+    
+      }
+      next()
+    }
     //  work - 1 get data from database 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray()
         res.send(result)
+    })
+    app.post('/menu', verifyToken, verifyAdmin, async(req, res) => {
+      const item = req.body
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
+    app.delete('/menu/:id', async(req, res) => {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+    })
+    app.get('/menu/:id', async(req, res) => {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.findOne(query)
+      res.send(result)
+    })
+    app.put('/menu/:id', async(req, res) => {
+      const item = req.body
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: item
+      }
+      const result = await menuCollection.updateOne(query, updateDoc)
+      res.send(result)
     })
     // work - 2 get data from database
     app.get('/reviews', async(req, res) => {
@@ -45,8 +127,11 @@ async function run() {
       res.send(result)
     })
     // work - 4 get data from cartsCollection
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', verifyToken,  async(req, res) => {
       const email = req.query.email 
+      if(email !== req.user?.email){
+        return res.status(403).send({message: 'unathorizided Access'})
+      }
       const query = {email: email}
       const result = await cartsCollection.find(query).toArray()
       res.send(result)
@@ -58,6 +143,80 @@ async function run() {
       const result = await cartsCollection.deleteOne(query)
       res.send(result)
     })
+    // users collection work start here 
+    app.post('/users', async(req, res) => {
+      const user = req.body 
+      const email = user?.email
+      const query = {email: email}
+      const isExists = await usersCollection.findOne(query)
+      if(isExists) return res.send(isExists)
+        const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+    app.get('/users',verifyToken, verifyAdmin,  async(req, res) => {
+       const result = await usersCollection.find().toArray()
+       res.send(result)
+    })
+    app.delete('/users/:id', verifyToken, verifyAdmin,  async(req, res) => {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+    app.patch('/users/admin/:id', verifyToken,verifyAdmin,  async(req, res) => {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+    app.get('/users/admin/:email', verifyToken,  async(req, res) => {
+      const email = req.params.email
+      if(email !== req.user?.email){
+        return res.status(403).send({message: 'unauthorized Access'})
+      }
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      let admin = false
+      if(user){
+        admin = user?.role === 'admin'
+      }
+      res.send({admin})
+      
+    })
+    app.post('/create-payment-intent', verifyToken, async(req, res) => {
+      const {price} = req.body 
+      const amount = parseInt(price * 100)
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    })
+    app.post('/payments', verifyToken, async(req, res) => {
+      const payment = req.body
+      const paymentResult = await paymentsCollection.insertOne(payment)
+      const query = {_id: {
+        $in: payment?.cartIds.map(id => new ObjectId(id))
+      }}
+      const deleteResult = await cartsCollection.deleteMany(query)
+      res.send({paymentResult, deleteResult})
+      
+    })
+    // get data show on payment history 
+    app.get('/payments/:email', verifyToken, async(req, res) => {
+      const query = {email: req.params.email}
+      if(req?.user?.email !== req.params.email) return res.status(403).send({message: 'Forbidden Access'})
+        const result = await paymentsCollection.find(query).toArray()
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -66,6 +225,8 @@ async function run() {
     // await client.close();
   }
 }
+// payment gate way work start here 
+
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
